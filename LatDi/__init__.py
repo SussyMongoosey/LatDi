54cf75188f559bc274e8e50e20bf8095e3a256f6
SussyMongoosey <150016091+SussyMongoosey@users.noreply.github.com>
2023-11-09 22:38:23 +0100

    Update __init__.py
    Changing __init__ file, let's see if we can import now

--- LatDi/__init__.py
+++ LatDi/__init__.py
@@ -7,6 +7,5 @@ Original file is located at
     https://colab.research.google.com/drive/1TTfk-CtfdnB0BAfhzgBMLvwDlJYLBfEX
 """
 
-from LatDi import create_complex_feature
-from LatDi import squeeze_to_complex
-from LatDi import CDR
+import pandas
+import numpy
